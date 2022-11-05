eb4fee299912039dfc348b340aeec05da08e9d8f
Nika Kobaidze <nika.kobaidze@gmail.com>
2022-11-05 22:05:24 +0000

    [ADD] data info

---
+++ src/data.js
@@ -0,0 +1,171 @@
+const data = {
+    about: {
+        first_name: "nika",
+        last_name: "kobaidze",
+        mobile: "+995 555121239",
+        email: "nika.kobaidze@gmail.com",
+        city: "tbilisi",
+        address: "Gldani III Disctrict, Block 79B Flat(68) ",
+        links: {
+            facebook: "",
+            instagram: "",
+            github: "",
+            youtube: "",
+        },
+    },
+    experience: [
+        {
+            organization: "PHP Developer",
+            position: "Digital Governance Agency",
+            date_from: "2022",
+            date_to: "present",
+            job_desc: "Create and support web applications based on php.",
+        },
+        {
+            organization: "PHP Developer",
+            position: "Citadeli",
+            date_from: "2021",
+            date_to: "present",
+            job_desc: "Create and support company CRM system based on php.",
+        },
+        {
+            organization: "Odeo Soft",
+            position: "Python Developer",
+            date_from: "2020",
+            date_to: "2021",
+            job_desc:
+                "Create and develop crm applications/modules for client's business needs.",
+        },
+        {
+            organization: "Medison Ambulatories",
+            position: "PHP Developer",
+            date_from: "2018",
+            date_to: "2020",
+            job_desc:
+                "Create and support medical applications according business needs, implement a data integration processes.",
+        },
+        {
+            organization: "",
+            position: "",
+            date_from: "",
+            date_to: "",
+            job_desc: "",
+        },
+        {
+            organization: "JSC Silknet",
+            position: "VoIP Administrator",
+            date_from: "2017",
+            date_to: "2018",
+            job_desc:
+                "Instalation/Configuration and support VOIP based systems and services.",
+        },
+        {
+            organization: "State military scientific-technical center DELTA",
+            position: "Python Developer",
+            date_from: "2014",
+            date_to: "2017",
+            job_desc:
+                "Create and support new modules for ERP system according to the company’s needs",
+        },
+        {
+            organization: "LTD 'MyGPS'",
+            position: "Programmer",
+            date_from: "2014",
+            date_to: "2014",
+            job_desc:
+                "Hardware programming, create and develop new C/C++ applications for microcontrollers.",
+        },
+        {
+            organization: "LTD 'Geonet'",
+            position: "Junior Network Administrator",
+            date_from: "2011",
+            date_to: "2014",
+            job_desc: "Configuration and support network devices",
+        },
+        {
+            organization: "Georgian Railway",
+            position: "Ji Support",
+            date_from: "2010",
+            date_to: "2011",
+            job_desc: "Configuration, monitoring and support internal network",
+        },
+        {
+            organization: "LTD 'Comptawn'",
+            position: "IT Support",
+            date_from: "2011",
+            date_to: "2011",
+            job_desc: "Installing and configuration a computer software.",
+        },
+        {
+            organization: "A. Chikobava institute of linguistics",
+            position: "Helpdesk",
+            date_from: "2010",
+            date_to: "2011",
+            job_desc:
+                "Installing and configuration a computer hardware, software, systems, networks, printers and scanners.",
+        },
+    ],
+    education: [
+        {
+            univeristy: "Georgian technical university",
+            Faculty: "Faculty of systems and networks",
+            degree: "Master’s degree",
+            start_date: "2010",
+            end_date: "2012",
+        },
+        {
+            univeristy: "I. Javakhishvili Tbilisi state University",
+            Faculty: "Faculty of exact and natural sciences",
+            degree: "Bachelor’s degree",
+            start_date: "2006",
+            end_date: "2010",
+        },
+        {
+            univeristy: "Georgian technical university",
+            Faculty: "Faculty of transport and mechanical engineering",
+            degree: "Bachelor’s degree",
+            start_date: "2008",
+            end_date: "2012",
+        },
+        {
+            univeristy: "Information technologies academy",
+            Faculty: "Computer networks and systems",
+            degree: "Technical",
+            start_date: "2008",
+            end_date: "2010",
+        },
+    ],
+    skills: {
+        programming: [{ name: "", logo: "" }],
+        workflow: ["", ""],
+    },
+    interests: "bla bla bla",
+    awards: [
+        {
+            organization: "Georgian innovation and technology agency",
+            training: "Python programming training",
+        },
+        {
+            organization: "University of Georgia",
+            training: "Microelectronic and microcontroller programming",
+        },
+        {
+            organization: "Georgian data exchange agency",
+            training: " Cyber security",
+        },
+        {
+            organization: "Information technologies academy",
+            training: "Cisco Certified Network Associate – CCNA",
+        },
+        {
+            organization: "Cisco",
+            training: "Cisco Certified Network Associate Security ",
+        },
+        {
+            organization: "Web studio Lemons",
+            training: "Web programing course (HTML, CSS, PHP, MySQL)",
+        },
+    ],
+};
+
+export default data;
