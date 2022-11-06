3d385239ad5b55a1db784aae0c691e341db55319
Nika Kobaidze <nika.kobaidze@gmail.com>
2022-11-06 12:42:35 +0000

    [DONE] about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,13 @@
 import React from "react";
+import data from "../data";
 
 function About() {
+    const { about } = data;
+    const { pages } = data.about;
+    const { first_name, last_name, mobile, email, city, address, text } = about;
+
+    console.log(pages);
+
     return (
         <section
             className="resume-section p-3 p-lg-5 d-flex d-column"
@@ -8,52 +15,31 @@ function About() {
         >
             <div className="my-auto">
                 <h1 className="mb-0">
-                    kobaidze
-                    <span className="text-primary">nika</span>
+                    {last_name}
+                    <span className="text-primary">{first_name}</span>
                 </h1>
                 <div className="subheading mb-5">
-                    Tbilisi · Gldani III Disctrict, Block 79B · Flat(68) ·
-                    <a href="mailto:name@email.com">nika.kobaidze@gmail.com</a>
+                    {mobile} · {city} · {address} ·
+                    <a href="mailto:name@email.com">{email}</a>
                 </div>
-                <p className="mb-5">
-                    I am experienced in leveraging agile frameworks to provide a
-                    robust synopsis for high level overviews. Iterative
-                    approaches to corporate strategy foster collaborative
-                    thinking to further the overall value proposition.
-                </p>
+                <p className="mb-5">{text}</p>
                 <ul className="list-inline list-social-icons mb-0">
-                    <li className="list-inline-item">
-                        <a href="/">
-                            <span className="fa-stack fa-lg">
-                                <i className="fa fa-circle fa-stack-2x"></i>
-                                <i className="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                            </span>
-                        </a>
-                    </li>
-                    <li className="list-inline-item">
-                        <a href="/">
-                            <span className="fa-stack fa-lg">
-                                <i className="fa fa-circle fa-stack-2x"></i>
-                                <i className="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                            </span>
-                        </a>
-                    </li>
-                    <li className="list-inline-item">
-                        <a href="/">
-                            <span className="fa-stack fa-lg">
-                                <i className="fa fa-circle fa-stack-2x"></i>
-                                <i className="fa fa-linkedin fa-stack-1x fa-inverse"></i>
-                            </span>
-                        </a>
-                    </li>
-                    <li className="list-inline-item">
-                        <a href="/">
-                            <span className="fa-stack fa-lg">
-                                <i className="fa fa-circle fa-stack-2x"></i>
-                                <i className="fa fa-github fa-stack-1x fa-inverse"></i>
-                            </span>
-                        </a>
-                    </li>
+                    {pages.map((page, id) => {
+                        const { name, link } = page;
+
+                        return (
+                            <li className="list-inline-item" key={id}>
+                                <a href={link} target="_blank">
+                                    <span className="fa-stack fa-lg">
+                                        <i className="fa fa-circle fa-stack-2x"></i>
+                                        <i
+                                            className={`fa fa-${name} fa-stack-1x fa-inverse`}
+                                        ></i>
+                                    </span>
+                                </a>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         </section>
