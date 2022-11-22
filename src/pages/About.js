51bae24beb13745d0868071e07b6dc27f87c751e
nkobaidze <nika.kobaidze@gmail.com>
2022-11-22 12:18:37 +0400

    [ADD] request sender

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,14 +8,41 @@ function About() {
 
     console.log(pages);
 
+    const sendRequest = () => {
+        const url = "http://45.9.46.62:8887/api/smsCheck.php";
+
+        const myHeaders = new Headers();
+
+        myHeaders.append(
+            "x-smstoken",
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9jaXRhZGVsaS5jb20iLCJhdWQiOiJodHRwOlwvXC9jaXRhZGVsaS5jb20iLCJpYXQiOjE2NjkxMDEyMzEsIm5iZiI6MTY2OTEwMTIzMSwiZXhwIjoxNjY5MTAxNDExLCJkYXRhIjp7ImlwIjoiNDUuOS40Ni42MiIsImlwX2xvY2F0aW9uIjoiZ2xvYmFsIiwib3NfYnJvd3NlciI6IlBvc3RtYW5SdW50aW1lXC83LjI5LjIiLCJhY3RpdmF0ZSI6MSwiaWQiOiI0NjkiLCJpZF9zdGFmZiI6IjQxOSIsInVzZXJuYW1lIjoibi5rb2JhaWR6ZSIsImZ1bGxuYW1lIjoiXHUxMGRjXHUxMGQ4XHUxMGQ5XHUxMGQwIFx1MTBkOVx1MTBkZFx1MTBkMVx1MTBkMFx1MTBkOFx1MTBlYlx1MTBkNCIsImNhdCI6IjEiLCJkZWZhdWx0X2lkX21vZHVsaSI6IjI3NSIsIm11bHRpX2FjY2VzcyI6IjAiLCJzbXNfY29kZSI6IjAyOTQifX0.Va0eW_PUrnCRPT2XFovMyz2HwtWpaQKws0OPyGYBbh8"
+        );
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: "",
+            redirect: "follow",
+        };
+
+        fetch(url, requestOptions)
+            .then((response) => response.text())
+            .then((result) => console.log(result))
+            .catch((error) => console.log("error", error));
+    };
+
     return (
         <section
             className="resume-section p-3 p-lg-5 d-flex d-column"
             id="about"
         >
             <div className="my-auto">
+                <button className="btn" onClick={sendRequest}>
+                    Send request
+                </button>
                 <h1 className="mb-0">
                     <span>{last_name} </span>
+
                     <span className="text-primary">{first_name}</span>
                 </h1>
                 <div className="subheading mb-5">
