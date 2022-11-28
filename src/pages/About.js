c6f0aacba8fb6c6a73c7854b5b39e5edf376546f
nkobaidze <nika.kobaidze@gmail.com>
2022-11-28 12:19:08 +0400

    [DEL] remove button

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,14 +9,14 @@ function About() {
     console.log(pages);
 
     const sendRequest = () => {
-        const url = "http://45.9.46.62:8887/api/index.php";
+        const url = "https://192.168.51.171";
 
         const myHeaders = new Headers();
 
-        myHeaders.append(
-            "x-smstoken",
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9jaXRhZGVsaS5jb20iLCJhdWQiOiJodHRwOlwvXC9jaXRhZGVsaS5jb20iLCJpYXQiOjE2NjkxMDEyMzEsIm5iZiI6MTY2OTEwMTIzMSwiZXhwIjoxNjY5MTAxNDExLCJkYXRhIjp7ImlwIjoiNDUuOS40Ni42MiIsImlwX2xvY2F0aW9uIjoiZ2xvYmFsIiwib3NfYnJvd3NlciI6IlBvc3RtYW5SdW50aW1lXC83LjI5LjIiLCJhY3RpdmF0ZSI6MSwiaWQiOiI0NjkiLCJpZF9zdGFmZiI6IjQxOSIsInVzZXJuYW1lIjoibi5rb2JhaWR6ZSIsImZ1bGxuYW1lIjoiXHUxMGRjXHUxMGQ4XHUxMGQ5XHUxMGQwIFx1MTBkOVx1MTBkZFx1MTBkMVx1MTBkMFx1MTBkOFx1MTBlYlx1MTBkNCIsImNhdCI6IjEiLCJkZWZhdWx0X2lkX21vZHVsaSI6IjI3NSIsIm11bHRpX2FjY2VzcyI6IjAiLCJzbXNfY29kZSI6IjAyOTQifX0.Va0eW_PUrnCRPT2XFovMyz2HwtWpaQKws0OPyGYBbh8"
-        );
+        // myHeaders.append(
+        //     "x-smstoken",
+        //     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9jaXRhZGVsaS5jb20iLCJhdWQiOiJodHRwOlwvXC9jaXRhZGVsaS5jb20iLCJpYXQiOjE2NjkxMDEyMzEsIm5iZiI6MTY2OTEwMTIzMSwiZXhwIjoxNjY5MTAxNDExLCJkYXRhIjp7ImlwIjoiNDUuOS40Ni42MiIsImlwX2xvY2F0aW9uIjoiZ2xvYmFsIiwib3NfYnJvd3NlciI6IlBvc3RtYW5SdW50aW1lXC83LjI5LjIiLCJhY3RpdmF0ZSI6MSwiaWQiOiI0NjkiLCJpZF9zdGFmZiI6IjQxOSIsInVzZXJuYW1lIjoibi5rb2JhaWR6ZSIsImZ1bGxuYW1lIjoiXHUxMGRjXHUxMGQ4XHUxMGQ5XHUxMGQwIFx1MTBkOVx1MTBkZFx1MTBkMVx1MTBkMFx1MTBkOFx1MTBlYlx1MTBkNCIsImNhdCI6IjEiLCJkZWZhdWx0X2lkX21vZHVsaSI6IjI3NSIsIm11bHRpX2FjY2VzcyI6IjAiLCJzbXNfY29kZSI6IjAyOTQifX0.Va0eW_PUrnCRPT2XFovMyz2HwtWpaQKws0OPyGYBbh8"
+        // );
 
         const requestOptions = {
             method: "POST",
@@ -46,11 +46,11 @@ function About() {
                     {city} · {address} · {mobile} ·{" "}
                     <a href="mailto:name@email.com">{email}</a>
                 </div>
-                <div>
+                {/* <div>
                     <button className="btn" onClick={sendRequest}>
                         Send request
                     </button>
-                </div>
+                </div> */}
                 <p className="mb-5">{text}</p>
                 <ul className="list-inline list-social-icons mb-0">
                     {pages.map((page, id) => {
